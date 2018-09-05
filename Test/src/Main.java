8db36189a06b956b4b153a74d7868557cf48ad89
Tarmo Kullas <Tarmo Kullas@DESKTOP-17VAHU0.lan>
2018-09-05 17:29:29 +0300

    Esimenene commit. Test programm.

---
+++ Test/src/Main.java
@@ -0,0 +1,16 @@
+//Seadistage oma IDE Gitiga töötama ja muutke muutujad nimi2 ning nimi3 oma nimedeks. Seejärel commitige.
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nimi1 = "Tarmo";
+		String nimi2 = "liige 2"; //muutmiseks
+		String nimi3 = "liige 3"; //muutmiseks
+		
+		System.out.printf("Seda programmi on Giti abil muutnud %s, %s ja %s.", nimi1, nimi2, nimi3);
+
+	}
+
+}
