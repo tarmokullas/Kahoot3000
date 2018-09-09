c492f137a75b161c5ef34cfaa119640f6024f772
taavi224 <taavi224@gmail.com>
2018-09-09 19:17:52 +0300

    Installed Spring Boot

---
+++ src/main/java/com/group4/Kahoot3000/Kahoot3000Application.java
@@ -0,0 +1,12 @@
+package com.group4.Kahoot3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kahoot3000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kahoot3000Application.class, args);
+	}
+}
---
+++ src/main/java/com/group4/Kahoot3000/MainController.java
@@ -0,0 +1,13 @@
+package com.group4.Kahoot3000;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
