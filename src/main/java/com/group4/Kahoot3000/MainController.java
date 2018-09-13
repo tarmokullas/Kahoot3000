4c481e3e48def97e2790e02ba65de051d3d6d4cb
Taavi Reisenbuk <taavi224@gmail.com>
2018-09-13 03:10:34 +0300

    Finally got the basics to work properly.

--- src/main/java/com/group4/Kahoot3000/MainController.java
+++ src/main/java/com/group4/Kahoot3000/MainController.java
@@ -4,29 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-
 
 @Controller
 public class MainController {
-
-
-
-    private static final String TEMPLATE_MAIN= "static/main";
-
-
-
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-
-    public String greeting(
-
-            @RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-
-        model.addAttribute("name", name);
-
-        return TEMPLATE_MAIN;
-
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    public String index(Model model) {
+        return "main";
     }
 }
