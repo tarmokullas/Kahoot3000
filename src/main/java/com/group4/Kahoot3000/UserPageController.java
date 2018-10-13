ca0f763618afdc92a1bb12af989f9c4be16ae534
Tarmo Kullas <tarmokullas@gmail.com>
2018-10-13 22:51:39 +0300

    Addition to previous commit. Missed some files

---
+++ src/main/java/com/group4/Kahoot3000/UserPageController.java
@@ -0,0 +1,16 @@
+package com.group4.Kahoot3000;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserPageController {
+
+    @RequestMapping(value = {"/userpager"}, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        return "userpage";
+    }
+}
