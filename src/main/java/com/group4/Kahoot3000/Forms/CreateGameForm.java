cc2e2fdaf571fbb90826f2fed50b47385b1c9455
Tarmo Kullas <tarmokullas@gmail.com>
2018-11-19 00:32:38 +0200

    Mängu loomiseks tehtud uus create.html fail, millest saab info kätte thymeleafiga. Hetkel on võimalik lisada 5 küsimust, kõik on kohustuslikud. Vajab CSS-i.

--- src/main/java/com/group4/Kahoot3000/Forms/CreateGameForm.java
+++ src/main/java/com/group4/Kahoot3000/Forms/CreateGameForm.java
@@ -4,84 +4,67 @@ public class CreateGameForm {
 
     //------------------FIRST--------------------
     private String question1;
-
-    private int gamePinCode;
+    private String question1trueAnswer;
 
     private String answer11;
-    private Boolean answer11isTrue;
 
     private String answer12;
-    private Boolean answer12isTrue;
 
     private String answer13;
-    private Boolean answer13isTrue;
 
     private String answer14;
-    private Boolean answer14isTrue;
 
 //------------------SECOND--------------------
 
     private String question2;
+    private String question2trueAnswer;
 
     private String answer21;
-    private Boolean answer21isTrue;
 
     private String answer22;
-    private Boolean answer22isTrue;
 
     private String answer23;
-    private Boolean answer23isTrue;
 
     private String answer24;
-    private Boolean answer24isTrue;
 
     //------------------THIRD--------------------
 
     private String question3;
+    private String question3trueAnswer;
 
     private String answer31;
-    private Boolean answer31isTrue;
 
     private String answer32;
-    private Boolean answer32isTrue;
 
     private String answer33;
-    private Boolean answer33isTrue;
 
     private String answer34;
-    private Boolean answer34isTrue;
 
     //-----------------FOURTH---------------------
 
     private String question4;
+    private String question4trueAnswer;
 
     private String answer41;
-    private Boolean answer41isTrue;
 
     private String answer42;
-    private Boolean answer42isTrue;
 
     private String answer43;
-    private Boolean answer43isTrue;
 
     private String answer44;
-    private Boolean answer44isTrue;
 
     //-----------------FIVE---------------------
 
     private String question5;
+    private String question5trueAnswer;
 
     private String answer51;
-    private Boolean answer51isTrue;
 
     private String answer52;
-    private Boolean answer52isTrue;
 
     private String answer53;
-    private Boolean answer53isTrue;
 
     private String answer54;
-    private Boolean answer54isTrue;
 
 
     public String getQuestion1() {
@@ -92,20 +75,20 @@ public class CreateGameForm {
         this.question1 = question1;
     }
 
+    public String getQuestion1trueAnswer() {
+        return question1trueAnswer;
+    }
+
+    public void setQuestion1trueAnswer(String question1trueAnswer) {
+        this.question1trueAnswer = question1trueAnswer;
+    }
+
     public String getAnswer11() {
         return answer11;
     }
 
     public void setAnswer11(String answer11) {
         this.answer11 = answer11;
-    }
-
-    public Boolean getAnswer11isTrue() {
-        return answer11isTrue;
-    }
-
-    public void setAnswer11isTrue(Boolean answer11isTrue) {
-        this.answer11isTrue = answer11isTrue;
     }
 
     public String getAnswer12() {
@@ -116,28 +99,12 @@ public class CreateGameForm {
         this.answer12 = answer12;
     }
 
-    public Boolean getAnswer12isTrue() {
-        return answer12isTrue;
-    }
-
-    public void setAnswer12isTrue(Boolean answer12isTrue) {
-        this.answer12isTrue = answer12isTrue;
-    }
-
     public String getAnswer13() {
         return answer13;
     }
 
     public void setAnswer13(String answer13) {
         this.answer13 = answer13;
-    }
-
-    public Boolean getAnswer13isTrue() {
-        return answer13isTrue;
-    }
-
-    public void setAnswer13isTrue(Boolean answer13isTrue) {
-        this.answer13isTrue = answer13isTrue;
     }
 
     public String getAnswer14() {
@@ -148,20 +115,20 @@ public class CreateGameForm {
         this.answer14 = answer14;
     }
 
-    public Boolean getAnswer14isTrue() {
-        return answer14isTrue;
-    }
-
-    public void setAnswer14isTrue(Boolean answer14isTrue) {
-        this.answer14isTrue = answer14isTrue;
-    }
-
     public String getQuestion2() {
         return question2;
     }
 
     public void setQuestion2(String question2) {
         this.question2 = question2;
+    }
+
+    public String getQuestion2trueAnswer() {
+        return question2trueAnswer;
+    }
+
+    public void setQuestion2trueAnswer(String question2trueAnswer) {
+        this.question2trueAnswer = question2trueAnswer;
     }
 
     public String getAnswer21() {
@@ -172,28 +139,12 @@ public class CreateGameForm {
         this.answer21 = answer21;
     }
 
-    public Boolean getAnswer21isTrue() {
-        return answer21isTrue;
-    }
-
-    public void setAnswer21isTrue(Boolean answer21isTrue) {
-        this.answer21isTrue = answer21isTrue;
-    }
-
     public String getAnswer22() {
         return answer22;
     }
 
     public void setAnswer22(String answer22) {
         this.answer22 = answer22;
-    }
-
-    public Boolean getAnswer22isTrue() {
-        return answer22isTrue;
-    }
-
-    public void setAnswer22isTrue(Boolean answer22isTrue) {
-        this.answer22isTrue = answer22isTrue;
     }
 
     public String getAnswer23() {
@@ -204,28 +155,12 @@ public class CreateGameForm {
         this.answer23 = answer23;
     }
 
-    public Boolean getAnswer23isTrue() {
-        return answer23isTrue;
-    }
-
-    public void setAnswer23isTrue(Boolean answer23isTrue) {
-        this.answer23isTrue = answer23isTrue;
-    }
-
     public String getAnswer24() {
         return answer24;
     }
 
     public void setAnswer24(String answer24) {
         this.answer24 = answer24;
-    }
-
-    public Boolean getAnswer24isTrue() {
-        return answer24isTrue;
-    }
-
-    public void setAnswer24isTrue(Boolean answer24isTrue) {
-        this.answer24isTrue = answer24isTrue;
     }
 
     public String getQuestion3() {
@@ -236,20 +171,20 @@ public class CreateGameForm {
         this.question3 = question3;
     }
 
+    public String getQuestion3trueAnswer() {
+        return question3trueAnswer;
+    }
+
+    public void setQuestion3trueAnswer(String question3trueAnswer) {
+        this.question3trueAnswer = question3trueAnswer;
+    }
+
     public String getAnswer31() {
         return answer31;
     }
 
     public void setAnswer31(String answer31) {
         this.answer31 = answer31;
-    }
-
-    public Boolean getAnswer31isTrue() {
-        return answer31isTrue;
-    }
-
-    public void setAnswer31isTrue(Boolean answer31isTrue) {
-        this.answer31isTrue = answer31isTrue;
     }
 
     public String getAnswer32() {
@@ -260,28 +195,12 @@ public class CreateGameForm {
         this.answer32 = answer32;
     }
 
-    public Boolean getAnswer32isTrue() {
-        return answer32isTrue;
-    }
-
-    public void setAnswer32isTrue(Boolean answer32isTrue) {
-        this.answer32isTrue = answer32isTrue;
-    }
-
     public String getAnswer33() {
         return answer33;
     }
 
     public void setAnswer33(String answer33) {
         this.answer33 = answer33;
-    }
-
-    public Boolean getAnswer33isTrue() {
-        return answer33isTrue;
-    }
-
-    public void setAnswer33isTrue(Boolean answer33isTrue) {
-        this.answer33isTrue = answer33isTrue;
     }
 
     public String getAnswer34() {
@@ -292,20 +211,20 @@ public class CreateGameForm {
         this.answer34 = answer34;
     }
 
-    public Boolean getAnswer34isTrue() {
-        return answer34isTrue;
-    }
-
-    public void setAnswer34isTrue(Boolean answer34isTrue) {
-        this.answer34isTrue = answer34isTrue;
-    }
-
     public String getQuestion4() {
         return question4;
     }
 
     public void setQuestion4(String question4) {
         this.question4 = question4;
+    }
+
+    public String getQuestion4trueAnswer() {
+        return question4trueAnswer;
+    }
+
+    public void setQuestion4trueAnswer(String question4trueAnswer) {
+        this.question4trueAnswer = question4trueAnswer;
     }
 
     public String getAnswer41() {
@@ -316,28 +235,12 @@ public class CreateGameForm {
         this.answer41 = answer41;
     }
 
-    public Boolean getAnswer41isTrue() {
-        return answer41isTrue;
-    }
-
-    public void setAnswer41isTrue(Boolean answer41isTrue) {
-        this.answer41isTrue = answer41isTrue;
-    }
-
     public String getAnswer42() {
         return answer42;
     }
 
     public void setAnswer42(String answer42) {
         this.answer42 = answer42;
-    }
-
-    public Boolean getAnswer42isTrue() {
-        return answer42isTrue;
-    }
-
-    public void setAnswer42isTrue(Boolean answer42isTrue) {
-        this.answer42isTrue = answer42isTrue;
     }
 
     public String getAnswer43() {
@@ -348,28 +251,12 @@ public class CreateGameForm {
         this.answer43 = answer43;
     }
 
-    public Boolean getAnswer43isTrue() {
-        return answer43isTrue;
-    }
-
-    public void setAnswer43isTrue(Boolean answer43isTrue) {
-        this.answer43isTrue = answer43isTrue;
-    }
-
     public String getAnswer44() {
         return answer44;
     }
 
     public void setAnswer44(String answer44) {
         this.answer44 = answer44;
-    }
-
-    public Boolean getAnswer44isTrue() {
-        return answer44isTrue;
-    }
-
-    public void setAnswer44isTrue(Boolean answer44isTrue) {
-        this.answer44isTrue = answer44isTrue;
     }
 
     public String getQuestion5() {
@@ -380,20 +267,20 @@ public class CreateGameForm {
         this.question5 = question5;
     }
 
+    public String getQuestion5trueAnswer() {
+        return question5trueAnswer;
+    }
+
+    public void setQuestion5trueAnswer(String question5trueAnswer) {
+        this.question5trueAnswer = question5trueAnswer;
+    }
+
     public String getAnswer51() {
         return answer51;
     }
 
     public void setAnswer51(String answer51) {
         this.answer51 = answer51;
-    }
-
-    public Boolean getAnswer51isTrue() {
-        return answer51isTrue;
-    }
-
-    public void setAnswer51isTrue(Boolean answer51isTrue) {
-        this.answer51isTrue = answer51isTrue;
     }
 
     public String getAnswer52() {
@@ -404,14 +291,6 @@ public class CreateGameForm {
         this.answer52 = answer52;
     }
 
-    public Boolean getAnswer52isTrue() {
-        return answer52isTrue;
-    }
-
-    public void setAnswer52isTrue(Boolean answer52isTrue) {
-        this.answer52isTrue = answer52isTrue;
-    }
-
     public String getAnswer53() {
         return answer53;
     }
@@ -420,35 +299,11 @@ public class CreateGameForm {
         this.answer53 = answer53;
     }
 
-    public Boolean getAnswer53isTrue() {
-        return answer53isTrue;
-    }
-
-    public void setAnswer53isTrue(Boolean answer53isTrue) {
-        this.answer53isTrue = answer53isTrue;
-    }
-
     public String getAnswer54() {
         return answer54;
     }
 
     public void setAnswer54(String answer54) {
         this.answer54 = answer54;
-    }
-
-    public Boolean getAnswer54isTrue() {
-        return answer54isTrue;
-    }
-
-    public void setAnswer54isTrue(Boolean answer54isTrue) {
-        this.answer54isTrue = answer54isTrue;
-    }
-
-    public int getGamePinCode() {
-        return gamePinCode;
-    }
-
-    public void setGamePinCode(int gamePinCode) {
-        this.gamePinCode = gamePinCode;
     }
 }
