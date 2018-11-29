9f3543f1136348f8a359590719772c5203de7edb
SergeiEensalu <sergei.eensalu@gmail.com>
2018-11-29 06:44:16 +0200

    small fix

--- src/main/resources/static/scripts/map.js
+++ src/main/resources/static/scripts/map.js
@@ -6,3 +6,16 @@ function myMap() {
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
     var marker = new google.maps.Marker({position: {lat: 58.3777964, lng: 26.7146504}, map: map});
 }
+
+
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
